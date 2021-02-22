4097f4e1119f1569d9c4e22ac4332fde69926d64
geoff <geoff@LAPTOP-M64G9J56>
2021-02-22 21:02:05 +0530

    commit

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  formModel = {
+    Username: '',
+    Password: ''
+  }
 
-  constructor() { }
+
+  constructor(public userService:UserService,private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  onSubmit(form: NgForm) {
+   
+
+    this.userService.login((form.value)).subscribe(
+      (res: any) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigateByUrl('/home');
+      },
+      err => {
+        if (err.status == 400)
+        alert('Incorect username and pasword')
+         // this.toastr.error('Incorrect username or password.', 'Authentication failed.');
+        else
+          console.log(err);
+      }
+    );
   }
 
 }
